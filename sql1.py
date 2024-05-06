19abeceb2641279f3c58499cb328287f572b9b23
dkesavapriya <138356423+dkesavapriya@users.noreply.github.com>
2024-05-06 06:16:25 +0530

    Add files via upload
    MYSQL Installation Command:
    
        • sudo apt install mysql-server
        • sudo systemctl start mysql.service
        • sudo mysql
        • ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
        • exit
        • mysql -u root -p
    
    Connect DB Using Python
        • sudo pip install mysql-connector-python

---
+++ sql1.py
@@ -0,0 +1,63 @@
+#TO CREATE NEW DATABASES
+
+import mysql.connector
+mydb=mysql.connector.connect(host="localhost",user="username",password="your password",database="stduManage")
+mycursor=mydb.cursor()
+
+def createDatabase(dbName):
+	sql="CREATE DATABASE "+dbName;
+	mycursor.execute(sql)
+createDatabase("stduManage")
+
+
+
+#TO CREATE A TABLE IN THAT DATABASE
+def createStudenttable():
+	sql= "create table student(id int,name varchar(255),age int,primary key(id))"
+	mycursor.execute(sql)
+createStudenttable()
+
+#TO INSERT VALUES 
+
+def insertValuesIntoTables():
+	sql="insert into student(id,name,age)values(%s,%s,%s)"
+	values=(int(1),'VGLUG',int(11))
+	mycursor.execute(sql,values)
+	mydb.commit()
+insertValuesIntoTables()
+
+#TO INSERT MULTIPLE VALUES
+
+def insertMultiValuesIntoTable():
+	sql="insert into student(id,name,age)values(%s,%s,%s)"
+	values=[(int(2),"john",int(24)),(int(7),"Mahi",int(42))]
+	mycursor.executemany(sql,values)
+	mydb.commit()
+insertMultiValuesIntoTable()
+
+
+#TO UPDATE THE VALUES
+
+def updateValues():
+	sql="update student set age='25' where name='john'"
+	mycursor.execute(sql)
+	mydb.commit()
+updateValues()
+
+
+#TO DELETE VALUES
+
+def deleteValuesIntoTable():
+	sql='delete from student where name = "john"'
+	mycursor.execute(sql)
+	mydb.commit()
+deleteValuesIntoTable()
+
+#TO FETCH ALL INFORMATION FROM THE TABLE
+
+def fetchAllFromTable():
+	sql="select * from student"
+	mycursor.execute(sql)
+	print(mycursor.fetchall())
+fetchAllFromTable()
+
